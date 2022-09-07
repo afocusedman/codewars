ecee5c5666ce8237c8b14484ba0fde9cb10b33f3
Thomas Maghanga <thomasmaghanga003@gmail.com>
2022-09-07 07:24:46 +0300

    Function to find name of planet using given ID

---
+++ name of planet.py
@@ -0,0 +1,33 @@
+"""
+Find name of the planet using given ID
+"""
+
+def get_planet_name(id):
+    # This doesn't work; Fix it!
+    name=""
+    if id == 1:
+        name="Mercury"
+        return name
+    elif id == 2:
+        name = "Venus"
+        return name
+    elif id == 3:
+        name = "Earth"
+        return name
+    elif id == 4:
+        name = "Mars"
+        return name
+    elif id == 5:
+        name = "Jupiter"
+        return name
+    elif id == 6:
+        name = "Saturn"
+        return name
+    elif id == 7:
+        name = "Uranus"
+        return name
+    elif id == 8:
+        name ="Neptune"
+        return name
+    return name
+    return 
